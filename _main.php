e73691548a7af6538eaf5eb461e49cf0c99c96e4
popskraft <popskraft@gmail.com>
2025-07-20 18:10:10 +0400

    chore: comment out unused frontend editor code and jQuery inclusion

--- _main.php
+++ _main.php
@@ -323,10 +323,10 @@ namespace ProcessWire;
   <?php
 
   // Jquery For admin fronend editor + editor code
-  if (page()->editable()) {
-    include_once "includes/editor-code.php";
-    echo "<script src='https://code.jquery.com/jquery-2.2.4.min.js' integrity='sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' crossorigin='anonymous'></script>";
-  }
+  // if (page()->editable()) {
+  //   include_once "includes/editor-code.php";
+  //   echo "<script src='https://code.jquery.com/jquery-2.2.4.min.js' integrity='sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' crossorigin='anonymous'></script>";
+  // }
 
   echo $siteCodeFooter;
 
